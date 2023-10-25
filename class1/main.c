32ee7eaacc51ef60817122b62adc8bef9cffb7f2
Kazi Khaled Nur <kazikhalednur@gmail.com>
2023-10-25 22:38:21 +0600

    first_class

---
+++ class1/main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+    int x, y;
+    // char a;
+    // a = 's';
+    // x = 23;
+    // x = (float)x;
+    // if(3 < 4){}
+    // if(!0){printf("OK\n");}
+    // for(int i=10; i < 0; i++)
+    // {
+    //     printf("%d", i);
+    // }
+    int i = 0;
+    while(i < 0){
+        i++;
+        print("%d", i);
+    }
+    return 0;
+}
---
+++ class1/main.py
@@ -0,0 +1,228 @@
+if 3 > 4:
+    pass
+# print("OK")
+# print("OK")
+# print("Ok3")
+
+
+"""print("OK")
+print("OK")
+# print("Ok3")
+
+
+
+rtu6tu
+
+rtyhtru
+
+hgtreuj
+
+
+dfghreh
+
+
+
+"""
+
+# data type
+int 
+float
+str
+
+
+
+# x = "sfreygherh"
+# y = "23.56"
+# print(x)
+# print(type(x))
+# print(y)
+# print(type(y))
+
+# y = str(y)
+# print(type(y))
+# print(y)
+
+# y34324543 = "hgtr"
+# print(y34324543)
+
+# x = "gre"
+# y = "grtty5e"
+# z = "grereyt54"
+
+# x, y, z = "fdsgf", "dfgrg", "thtrh"
+
+# print(x, y, z, end="")
+
+# if 23 > 3 and 45 < 546:
+# if 23 < 3 or 45 < 546:
+#     print("Right")
+# else:
+#     print("Not right")
+
+# if not 0:
+#     print("OK")
+
+# if 23 is not 0:
+#     print("OK")
+
+# x = ""
+# x = None
+
+# if x is not None:
+# if x != None:
+#     print("OK")
+
+
+# x = 23 // 4
+# print(x)
+
+# x = 23 % 4
+# print(x)
+
+# x = 3
+# x += 5 # x = x + 5
+# print(x)
+
+
+list
+set
+tuple
+
+
+
+
+
+#list
+
+# asd = ["34", "fgr", "gre", "fgr6"]
+
+# asd = tuple(asd)
+
+# asd.pop()
+# print(type(asd))
+
+# # asd.append("rtr")
+# asd.insert(0, "rtr")
+
+# # asd.sort()
+# # asd = sorted(asd)
+# # asd.pop(3)
+
+# asd = None
+# print(type(asd))
+# asd.clear()
+
+# for i in asd:
+#     print(i)
+
+
+# print(len(asd))
+
+
+
+
+
+
+# set
+
+# ads = ["rtre", "rtre"]
+# ads = set(ads)
+# print(ads)
+# print(type(ads))
+
+# tuple
+
+# asd = ("fasf", "sdf", "sdfer")
+# # asd[0] = "grthg"
+# # asd.append("dsf")
+# # asd.pop()
+# # asd.clear()
+
+
+# # asd[0] = 0
+
+# asd = list(asd)
+# asd.pop()
+
+# print(asd)
+
+
+
+
+dict
+
+# dictionary
+
+# asdd = {
+#     1: "dfgtrhtrhjtyjh",
+#     "adf5" : "grfgr"
+# }
+
+# asdd["fdg"] = "ghtr"
+# asdd["adf5"] = "regr"
+# asdd[1] = "grtyh"
+
+# print(asdd.keys())
+
+# # print(asdd[1])
+
+# # for i in asdd.keys():
+# for i in asdd:
+#     print(asdd[i])
+
+
+
+
+
+
+# nested dictionary
+
+# aasdf = {
+#     "asdf": {
+#         "daf": "fgf"
+#     },
+#     "asdf": "frefg"
+# }
+
+# asdd = aasdf
+
+# # print(aasdf["asdf"]["daf"])
+# print(asdd)
+
+
+
+
+# for loop
+# sdf = ["gfgg", "hgreg", "ghrg"]
+# sdf = "sdfdsf"
+
+# for asd in sdf:
+#     print(asd)
+
+# for ads in range(1, 10, 2):
+#     # if ads == 5:
+#     #     continue
+#     # print(ads)
+
+#     if ads == 5:
+#         break
+#     else:
+#         print(ads)
+
+
+
+
+
+
+
+
+
+
+
+# while loop
+
+# asd = 0
+# while asd < 5:
+#     print(asd)
+#     asd += 1 # asd = asd - 1
+
